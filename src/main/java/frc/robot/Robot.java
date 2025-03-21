ddf3075f0cd377057c9b3e0d61366d4393c08218
Layala8839 <ayal3062@vandals.uidaho.edu>
2025-03-21 04:26:11 -0700

    Updated Robot Container

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,24 +4,50 @@
 
 package frc.robot;
 
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.cscore.UsbCamera;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 public class Robot extends TimedRobot {
+
+  public UsbCamera usbCamera1;
+  public UsbCamera usbCamera2;
+
   private Command m_autonomousCommand;
 
   private final RobotContainer m_robotContainer;
 
-  private final boolean kUseLimelight = false;
+  private final boolean kUseLimelight = true;
 
   public Robot() {
     m_robotContainer = new RobotContainer();
-  }
+    // Instantiate our RobotContainer.  This will perform all our button bindings
+    //USB camera 1
+    try {
+      usbCamera1 = CameraServer.startAutomaticCapture(0);
+        } catch (Exception e) {
+          SmartDashboard.putString("camera 0 capture failed","failed");
+        }
+    //Usb camera 2
+        try {
+      usbCamera2 = CameraServer.startAutomaticCapture(1);
+        } catch (Exception e) {
+          SmartDashboard.putString("camera 1 capture failed","failed");
+        }
+      }
+
 
   @Override
   public void robotPeriodic() {
+    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
+    // commands, running already-scheduled commands, removing finished or interrupted commands,
+    // and running subsystem periodic() methods.  This must be called from the robot's periodic
+    // block in order for anything in the Command-based framework to work.
+
     CommandScheduler.getInstance().run(); 
 
     /*
@@ -32,6 +58,7 @@ public class Robot extends TimedRobot {
      * This example is sufficient to show that vision integration is possible, though exact implementation
      * of how to use vision should be tuned per-robot and to the team's specification.
      */
+    
     if (kUseLimelight) {
       var driveState = m_robotContainer.drivetrain.getState();
       double headingDeg = driveState.Pose.getRotation().getDegrees();
@@ -54,15 +81,18 @@ public class Robot extends TimedRobot {
   @Override
   public void disabledExit() {}
 
+  /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
   @Override
   public void autonomousInit() {
     m_autonomousCommand = m_robotContainer.getAutonomousCommand();
-
+    
+    // schedule the autonomous command (example)
     if (m_autonomousCommand != null) {
       m_autonomousCommand.schedule();
     }
   }
 
+  /** This function is called periodically during autonomous. */
   @Override
   public void autonomousPeriodic() {}
 
@@ -71,11 +101,17 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopInit() {
+    // This makes sure that the autonomous stops running when
+    // teleop starts running. If you want the autonomous to
+    // continue until interrupted by another command, remove
+    // this line or comment it out.
     if (m_autonomousCommand != null) {
       m_autonomousCommand.cancel();
     }
   }
 
+
+  /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {}
 
@@ -87,12 +123,13 @@ public class Robot extends TimedRobot {
     CommandScheduler.getInstance().cancelAll();
   }
 
-  @Override
-  public void testPeriodic() {}
+  //@Override
+  // Cancels all running commands at the start of test mode.
+  //public void testPeriodic() {}
 
-  @Override
-  public void testExit() {}
+  //@Override
+  //public void testExit() {}
 
-  @Override
-  public void simulationPeriodic() {}
+  //@Override
+  //public void simulationPeriodic() {}
 }
